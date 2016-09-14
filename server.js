55792d88faccd134b9d058ad93a3946c3fa6259c
Lander Arnoys <landerarnoys@hotmail.com>
2016-09-14 11:28:45 +0200

    added index

--- server.js
+++ server.js
@@ -3,31 +3,35 @@ var express = require('express');
 var app = express();
 const wkhtmltopdf = require('node-wkhtmltopdf');
 var fs = require('fs');
-
-app.get('/pdfdownload', function (req, res) {
-
-    const options = [
-        '--margin-bottom 18',
-        '--margin-left 10',
-        '--margin-right 10',
-        '--margin-top 20',
-        '--custom-header Accept-Language fr-CA',
-    ];
-
-    var url = 'http://www.google.com';
-
-    const doc = wkhtmltopdf(options, url);
-
-    doc.stdout.pipe(res);
-
-    return res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Access-Control-Allow-Origin': '*',
-        'Content-Disposition': 'attachment; filename="test.pdf"',
-    });
+var path = require('path');
 
 
+// app.get('/pdfdownload', function (req, res) {
+//
+//     const options = [
+//         '--margin-bottom 18',
+//         '--margin-left 10',
+//         '--margin-right 10',
+//         '--margin-top 20',
+//         '--custom-header Accept-Language fr-CA',
+//     ];
+//
+//     var url = 'http://www.google.com';
+//
+//     const doc = wkhtmltopdf(options, url);
+//
+//     doc.stdout.pipe(res);
+//
+//     return res.writeHead(200, {
+//         'Content-Type': 'application/pdf',
+//         'Access-Control-Allow-Origin': '*',
+//         'Content-Disposition': 'attachment; filename="test.pdf"',
+//     });
+//
+// });
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 //start the server
