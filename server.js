a99c1f98fc3e8c118397ffc2263e14b305c08bb4
Lander Arnoys <landerarnoys@hotmail.com>
2016-09-14 13:23:47 +0200

    test

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/pdfdownload', function (req, res) {
         '--margin-left 10',
         '--margin-right 10',
         '--margin-top 20',
-        '--custom-header Accept-Language fr-CA',
+        '--custom-header authorization bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidXNlcjFAY3VzdG9tczR0cmFkZS5jb20iLCJlbWFpbCI6InVzZXIxQGN1c3RvbXM0dHJhZGUuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOi8vY2FzLWRldi5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NTQ5MmUxMTQ4ZGU3MDQ5ODc3ZDE1ZmQxIiwiYXVkIjoid0FNUHRVMkhWMEhtWFk5Y0ZWU0xsSzZ2Q2tIYkU5a2kiLCJleHAiOjE0NzM4ODgxNDUsImlhdCI6MTQ3Mzg1MjE0NX0.LjXu330GsQQVO7Yv8xvZ3F9S2r48STCJjrZcFDK9oyk'
     ];
 
-    var url = 'http://www.google.com';
+    var url = 'https://cas-dev.azurewebsites.net/#/dashboard';
 
     const doc = wkhtmltopdf(options, url);
 
