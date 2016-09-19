634d3e2c746b53de1fd171cf7814de6e85672f52
Lander Arnoys <landerarnoys@hotmail.com>
2016-09-19 12:28:44 +0200

    updated server

--- server/server.js
+++ server/server.js
@@ -946,8 +946,9 @@ app.get('/pdfdownload', function (req, res) {
     doc.stdout.pipe(res);
 
     return res.writeHead(200, {
+        // 'Content-Type': 'application/pdf',
         'Access-Control-Allow-Origin': '*',
-        'charset': 'utf-8'
+        // 'Content-Disposition': 'inline; filename="test.pdf"',
     });
 
 });
